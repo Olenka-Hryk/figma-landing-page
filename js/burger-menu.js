0e973b3be675a831ef59018896260cf75e433131
Olena Hryk <olena.hryk@gmail.com>
2023-05-22 21:00:28 +0300

    Added burger menu

---
+++ js/burger-menu.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("burgerMenu").style.width = "100%";
+    document.getElementById("burgerMenu-open").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("burgerMenu").style.width = "0%";
+    document.getElementById("burgerMenu-open").style.display = "block";
+}
